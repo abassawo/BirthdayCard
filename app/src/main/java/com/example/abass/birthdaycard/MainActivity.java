4f7fab739b758fadd1db716265e42dab0c3c0a7f
Abass <Abass@C4Qs-MacBook-Pro-9.local>
2016-09-25 12:37:39 -0400

    init commit

---
+++ app/src/main/java/com/example/abass/birthdaycard/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.abass.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayAdapter adapter;
+    Spinner spinner;
+    Spinner spinner2;
+    Integer[] days;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.month_spinner);
+        spinner2 = (Spinner) findViewById(R.id.day_spinner);
+        String[] months = getResources().getStringArray(R.array.string_array_months);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter = new ArrayAdapter(this, R.layout.text_layout, months);
+        spinner.setAdapter(adapter);
+
+
+        days = makeArrayOfDays();
+        ArrayAdapter dayAdapter = new ArrayAdapter(this, R.layout.text_layout, days);
+        spinner2.setAdapter(dayAdapter);
+    }
+
+    public Integer[] makeArrayOfDays(){
+        Integer[] numArr = new Integer[31];
+        for (int i = 0; i < numArr.length; i++){
+            numArr[i] = i+1;
+        }
+        return numArr;
+    }
+
+}
---
+++ app/src/main/java/com/example/abass/birthdaycard/MySpinnerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.abass.birthdaycard;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.SpinnerAdapter;
+
+/**
+ * Created by Abass on 9/25/16.
+ */
+
+public class MySpinnerAdapter extends BaseAdapter {
+    private String[] arr;
+
+
+    public MySpinnerAdapter(String[] arr){
+        this.arr = arr;
+    }
+
+    @Override
+    public int getCount() {
+        return arr.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arr[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        //textVView.setText(
+        return view;
+    }
+}
